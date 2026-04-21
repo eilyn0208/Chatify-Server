eb56f2f65ea58ff340a5be94cc4868c92966e139
Eilyn <eilynsresendezm@gmail.com>
2026-04-21 17:31:50 -0600

    credentials?

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const io = new Server(server, {
   cors: {
     origin: process.env.CLIENT_URL || 'http://localhost:5173',
     methods: ['GET', 'POST'],
-    credentials: true
   }
 });
 //PostgreSQL connection
